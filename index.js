ccf4c2f5a46db4d74f2c2aa9d082f5e05cc5f256
Salman-sajib <sfsajib@gmail.com>
2024-02-24 18:56:49 +0600

    setup an environment variable

--- index.js
+++ index.js
@@ -1,21 +1,16 @@
 // dependencies
 const http = require('node:http');
-
 const { handleReqRes } = require('./helpers/handleReqRes');
+const enviroment = require('./helpers/environments');
 
 // app object - module scaffolding
 const app = {};
 
-// configuration
-app.config = {
-  port: 3000,
-};
-
 // create server
 app.createServer = () => {
   const server = http.createServer(app.handleReqRes);
-  server.listen(app.config.port, () => {
-    console.log(`Listening to port ${app.config.port}`);
+  server.listen(enviroment.port, () => {
+    console.log(`Listening to port ${enviroment.port}`);
   });
 };
 
